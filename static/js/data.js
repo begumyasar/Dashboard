26bbc060923d0d0f629d43f0f6f183705023ca94
Wesleyvd <wesley.v.d@hotmail.nl>
2016-04-28 15:28:59 +0200

    example data for the graph

--- static/js/data.js
+++ static/js/data.js
@@ -1,35 +1,23 @@
 var data = {
-    labels: ["Maandag 25 april", "Dinsdag 26 april", "Woensdag 27 april", "Donderdag 28 april", "Vrijdag 29 april", "Zaterdag 30 april", "Zondag 1 mei"],
+    labels: ["00", "01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24"],
     datasets: [
-        {
-            label: "Voltaplein 55",
-
-            // The properties below allow an array to be specified to change the value of the item at the given index
-            // String  or array - the bar color
-            backgroundColor: "rgba(231,35,74, 0.5)",
-
-            // String or array - bar stroke color
-            borderColor: "rgba(231,35,74, 1)",
-
-            // Number or array - bar border width
+        {   // Aantal meldingen
+            label: "Voltastraat 1 (meldingen)",
+            backgroundColor: "rgba(255,100,100,0.5)",
+            borderColor: "rgba(255,0,0,1)",
             borderWidth: 3,
             hoverBackgroundColor: "rgba(53,64,82,0.5)",
             hoverBorderColor: "rgba(53,64,82,1)",
-
-            // The actual data
-            data: [4, 8, 2, 0, 6, 4, 7],
-
-            // String - If specified, binds the dataset to a certain y-axis. If not specified, the first y-axis is used.
-            yAxisID: "y-axis-0",
+            data: [3, 5, 2, 0, 0, 1, 0, 0, 0, 1, 5, 6, 4, 5, 3, 6, 5, 4, 3, 8, 12, 14, 19, 17, 6]
         },
-        {
-            label: "Voltastraat 1",
-            backgroundColor: "rgba(0,172,80,0.5)",
-            borderColor: "rgba(0,172,80,1)",
+        {   // Activity
+            label: "Voltastraat 1 (activity)",
+            backgroundColor: "rgba(100,100,255,0.5)",
+            borderColor: "rgba(0,0,255,1)",
             borderWidth: 3,
             hoverBackgroundColor: "rgba(53,64,82,0.5)",
             hoverBorderColor: "rgba(53,64,82,1)",
-            data: [2, 4, 4, 1, 8, 7, 9]
+            data: [5, 11, 4, 0, 0, 0, 3, 1, 0, 0, 4, 3, 1, 7, 15, 23, 14, 9, 15, 12, 16, 19, 21, 17, 13]
         }
     ]
 };
@@ -39,7 +27,7 @@ var data = {
 var ctx = document.getElementById("myChart");
 
 var myBarChart = new Chart(ctx, {
-    type: 'bar',
+    type: 'line',
     data: data,
     responsive: true,
     position: 'bottom',
