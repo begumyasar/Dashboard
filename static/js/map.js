2887f5d4a1e4a6ae8b537af33da6621ae7f11e61
Dennis van Bennekom <vanbennekomdennis@gmail.com>
2016-04-28 15:21:18 +0200

    map klaar

--- static/js/map.js
+++ static/js/map.js
@@ -1,22 +1,27 @@
 var marker1, marker2;
 var map;
 
+var icons = [
+    '/static/images/markers/0.png',
+    '/static/images/markers/1.png',
+    '/static/images/markers/2.png',
+    '/static/images/markers/3.png',
+    '/static/images/markers/4.png',
+    '/static/images/markers/5.png',
+    '/static/images/markers/6.png',
+    '/static/images/markers/7.png',
+    '/static/images/markers/8.png',
+    '/static/images/markers/9plus.png',
+];
+
+var data = [];
+var ldr1Data = 0;
+var ldr2Data = 0;
+    
+
 function initMap() {
     // 52.3533494,4.9424607
     // 52.3531429,4.9357809
-    
-    var icons = [
-        '/static/images/markers/1.png',
-        '/static/images/markers/2.png',
-        '/static/images/markers/3.png',
-        '/static/images/markers/4.png',
-        '/static/images/markers/5.png',
-        '/static/images/markers/6.png',
-        '/static/images/markers/7.png',
-        '/static/images/markers/8.png',
-        '/static/images/markers/9.png',
-        '/static/images/markers/9plus.png'
-    ];
     
     var styles = [
         {
@@ -68,23 +73,76 @@ function initMap() {
     });
     
     map.setOptions({styles: styles});
+    
+    info1 = new google.maps.InfoWindow();
+    info2 = new google.maps.InfoWindow();
+    
+    createMarkers(1, 4);
+    
+    updateData();
+    setInterval(function() {
+        updateData();
+    }, 10000);
 }
 
-function createMarkers(icon1, icon2) {
+function createMarkers(value1, value2) {
+    
     if (marker1 || marker2) {
         marker1.setMap(null);
         marker2.setMap(null);
     }
     
+    var icon1 = icons[value1];
+    var icon2 = icons[value2];
+    
     marker1 = new google.maps.Marker({
         position: { lat: 52.3533494, lng: 4.9424607 },
         map: map,
-        title: 'Sensor 1'
+        icon: icon1
     });
     
     marker2 = new google.maps.Marker({
         position: { lat: 52.3531429, lng: 4.9357809 },
         map: map,
-        title: 'Sensor 2'
+        icon: icon2
     });
+    
+    marker1.addListener('click', function() { 
+        window.location.href = 'esp1.html';
+    });
+    
+    marker2.addListener('click', function() {
+        window.location.href = 'esp2.html';
+    });
+}
+
+function updateData() {
+    var currentDate = new Date();
+    var day = currentDate.getUTCDate();
+    var month = currentDate.getUTCMonth() + 1;
+    var year = currentDate.getUTCFullYear();
+    
+    fetch('http://api.leandervanbaekel.nl/alarm/day/' + day + '/' + month + '/' + year + '/esp1/null')
+        .then(function(response) {
+           return response.json(); 
+        })
+        .then(function(data) {
+           ldr1Data = data.length;
+           
+           if (ldr1Data > 9) { ldr1Data = 9; }
+           
+           createMarkers(ldr1Data, ldr2Data);
+        });
+        
+    fetch('http://api.leandervanbaekel.nl/alarm/day/' + day + '/' + month + '/' + year + '/esp2/null')
+        .then(function(response) {
+           return response.json(); 
+        })
+        .then(function(data) {
+           ldr2Data = data.length;
+           
+           if (ldr2Data > 9) { ldr2Data = 9; }
+           
+           createMarkers(ldr1Data, ldr2Data);
+        });
 }
