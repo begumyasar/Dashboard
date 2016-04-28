4637a27492b1806d655a10c2ff6c910f3cc67a6f
Dennis van Bennekom <vanbennekomdennis@gmail.com>
2016-04-28 11:53:48 +0200

    update

--- static/js/map.js
+++ static/js/map.js
@@ -1,7 +1,7 @@
-(function() {
+function initMap() {
     var $map = document.getElementById('map');
     var map = new google.maps.Map($map, {
         center: { lat: 52.350315, lng: 4.888296 },
-        zoom: 8
+        zoom: 14
     });
-}());
+}
