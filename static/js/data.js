4a144547367bba655fc1c8de587705b955e85728
Sembah <macbook@134.120.028.145.hva.nl>
2016-04-29 10:05:46 +0200

    Made esp page layout

--- static/js/data.js
+++ static/js/data.js
@@ -2,35 +2,100 @@ var data = {
     labels: ["00", "01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24"],
     datasets: [
         {   // Aantal meldingen
-            label: "Voltastraat 1 (meldingen)",
+            label: "Voltaplein (meldingen)",
             backgroundColor: "rgba(255,100,100,0.5)",
             borderColor: "rgba(255,0,0,1)",
             borderWidth: 3,
             hoverBackgroundColor: "rgba(53,64,82,0.5)",
             hoverBorderColor: "rgba(53,64,82,1)",
             data: [3, 5, 2, 0, 0, 1, 0, 0, 0, 1, 5, 6, 4, 5, 3, 6, 5, 4, 3, 8, 12, 14, 19, 17, 6]
-        },
-        {   // Activity
-            label: "Voltastraat 1 (activity)",
-            backgroundColor: "rgba(100,100,255,0.5)",
-            borderColor: "rgba(0,0,255,1)",
-            borderWidth: 3,
-            hoverBackgroundColor: "rgba(53,64,82,0.5)",
-            hoverBorderColor: "rgba(53,64,82,1)",
-            data: [5, 11, 4, 0, 0, 0, 3, 1, 0, 0, 4, 3, 1, 7, 15, 23, 14, 9, 15, 12, 16, 19, 21, 17, 13]
         }
     ]
 };
 
 
 
-var ctx = document.getElementById("myChart");
+var voltaplein = document.getElementById("VoltapleinMelding");
 
-var myBarChart = new Chart(ctx, {
-    type: 'line',
+var myBarChart = new Chart(voltaplein, {
+    type: 'bar',
     data: data,
     responsive: true,
     position: 'bottom',
     defaultFontColor: "#FFFFFF"
 });
 
+var activity = {
+    labels: ["00", "01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24"],
+    datasets: [
+        {
+            label: "Voltaplein (Activiteit)",
+            backgroundColor: "rgba(255,100,100,0.5)",
+            borderColor: "rgba(255,0,0,1)",
+            borderWidth: 3,
+            hoverBackgroundColor: "rgba(53,64,82,0.5)",
+            hoverBorderColor: "rgba(53,64,82,1)",
+            data: [3, 5, 2, 0, 0, 1, 0, 0, 0, 1, 5, 6, 4, 5, 3, 6, 5, 4, 3, 8, 12, 14, 19, 17, 6]  
+        }
+    ]
+};
+
+var voltapleinActivity = document.getElementById("VoltapleinActivity");
+
+var myLineChart = new Chart(voltapleinActivity, {
+    type: 'line',
+    data: activity,
+    responsive: true,
+    position: 'bottom',
+    defaultFontColor: "#FFFFFF"
+});
+
+var sound = {
+    labels: ["00", "01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24"],
+    datasets: [
+        {
+            label: "Voltaplein (Sound)",
+            backgroundColor: "rgba(255,100,100,0.5)",
+            borderColor: "rgba(255,0,0,1)",
+            borderWidth: 3,
+            hoverBackgroundColor: "rgba(53,64,82,0.5)",
+            hoverBorderColor: "rgba(53,64,82,1)",
+            data: [3, 5, 2, 0, 0, 1, 0, 0, 0, 1, 5, 6, 4, 5, 3, 6, 5, 4, 3, 8, 12, 14, 19, 17, 6]  
+        }
+    ]
+};
+
+var voltapleinSound = document.getElementById("VoltapleinSound");
+
+var myLineChart = new Chart(voltapleinSound, {
+    type: 'line',
+    data: sound,
+    responsive: true,
+    position: 'bottom',
+    defaultFontColor: "#FFFFFF"
+});
+
+var light = {
+    labels: ["00", "01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24"],
+    datasets: [
+        {
+            label: "Voltaplein (Light)",
+            backgroundColor: "rgba(255,100,100,0.5)",
+            borderColor: "rgba(255,0,0,1)",
+            borderWidth: 3,
+            hoverBackgroundColor: "rgba(53,64,82,0.5)",
+            hoverBorderColor: "rgba(53,64,82,1)",
+            data: [3, 5, 2, 0, 0, 1, 0, 0, 0, 1, 5, 6, 4, 5, 3, 6, 5, 4, 3, 8, 12, 14, 19, 17, 6]  
+        }
+    ]
+};
+
+var voltapleinLight = document.getElementById("VoltapleinLight");
+
+var myLineChart = new Chart(voltapleinLight, {
+    type: 'line',
+    data: light,
+    responsive: true,
+    position: 'bottom',
+    defaultFontColor: "#FFFFFF"
+});
